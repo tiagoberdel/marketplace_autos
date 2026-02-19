859945e64cbbcb3339102e5211e20212507e18e9
tiagoberdel <tiagoberdel@gmail.com>
2026-02-19 18:32:51 -0300

    commit: arreglos de imports y archivo de db

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,21 @@
 from flask import Flask
-from config import Config
-from app.models.auto import db
+from app.database import db
 from app.routes.autos import autos_bp
+from flask_cors import CORS
 
 def crear_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///autos.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
-    app.register_blueprint(autos_bp)
+    CORS(app)
+
+    app.register_blueprint(autos_bp, url_prefix="/autos")
 
     with app.app_context():
         db.create_all()
 
-    @app.route("/")
-    def home():
-        return {"mensaje": "API corriendo"}
-    
     return app
--- app/models/auto.py
+++ app/models/auto.py
@@ -1,9 +1,19 @@
-from flask_sqlalchemy import SQLAlchemy
+from app.database import db
 
-db = SQLAlchemy()
 class Auto(db.Model):
+    __tablename__ = "autos"
+
     id = db.Column(db.Integer, primary_key=True)
-    marca = db.Column(db.String(30), nullable=False)
-    modelo = db.Column(db.String(30), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    precio = db.Column(db.Float, nullable=False)
+    marca = db.Column(db.String(50))
+    modelo = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+    precio = db.Column(db.Float)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "marca": self.marca,
+            "modelo": self.modelo,
+            "year": self.year,
+            "precio": self.precio,
+        }
